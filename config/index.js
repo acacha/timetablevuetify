ada814dc691b2f9dbb4dce6fc1a3f0cac36b8df0
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-04-12 15:32:56 +0200

    Sync

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/timetable_drag_and_drop',
 
     /**
      * Source Maps
